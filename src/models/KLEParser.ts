29c83c90923f8f5767b185c26929103a31e90686
Bryan Arendt <barendt@covermymeds.com>
2019-08-23 08:17:00 -0400

    add to parser

--- src/models/KLEParser.ts
+++ src/models/KLEParser.ts
@@ -1,4 +1,4 @@
-import { Keyboard, Serial } from "@ijprest/kle-serial";
+import { Key, Keyboard, Serial } from "@ijprest/kle-serial";
 
 export default class KLEParser {
   private source: string;
@@ -10,5 +10,46 @@ export default class KLEParser {
 
   parse() {
     return this.parsed;
+  }
+
+  keebParse(kle: Array<object | Array<object | string>>) {
+    const gridIndex = new Grid();
+    const keysOnly: Array<KeebKey> = [];
+    kle.forEach((row: Array<string | object> | object, rowIndex: number) => {
+      const isArray = Array.isArray(row);
+      gridIndex.col = -1;
+      if (isArray) {
+        gridIndex.row++;
+        const xx = row as Array<string | object>;
+        xx.forEach((key: string | object, keyIndex: number) => {
+          if (typeof key === "string") {
+            gridIndex.col++;
+            const keebKey = new KeebKey(key, gridIndex);
+            keysOnly.push(keebKey);
+          }
+        });
+      }
+    });
+
+    new KLEParser(JSON.stringify(kle))
+      .parse()
+      .keys.map((key: Key, index: number) => {
+        keysOnly[index].kleKey = key;
+      });
+  }
+}
+
+export class Grid {
+  col: number = -1;
+  row: number = -1;
+}
+export class KeebKey {
+  label: string = "";
+  kleKey?: Key;
+  gridIndex: Grid = new Grid();
+  constructor(key: string, gridIndex: Grid, kleKey?: Key) {
+    this.label = key;
+    this.gridIndex = gridIndex;
+    this.kleKey = kleKey;
   }
 }
