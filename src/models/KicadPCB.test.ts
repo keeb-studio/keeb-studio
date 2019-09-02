7bfc469dfd570f73a0d929183f2b5066dd24cdd5
Bryan Arendt <barendt@covermymeds.com>
2019-09-02 07:45:25 -0600

    can find root and sections

--- src/models/KicadPCB.test.ts
+++ src/models/KicadPCB.test.ts
@@ -1,234 +1,84 @@
-import KicadPCB, { CLOSE_CONTEXT, START } from "./KicadPCB";
+import { readFileSync } from "fs";
+import KicadPCB from "./KicadPCB";
 describe("KicadPCB", () => {
-  describe("addToken", () => {
-    describe("()", () => {
-      it("(", () => {
-        expect(
-          KicadPCB.addToken({
-            tokens: ["(", ")"],
-            action: START,
-            context: {},
-            property: "",
-            open: 0
-          })
-        ).toEqual({
-          action: CLOSE_CONTEXT,
-          context: {},
-          property: "",
-          tokens: [],
-          open: 0
-        });
-      });
-    });
-    describe("(kicad_pcb())", () => {
-      it("kicad_pcb", () => {
-        expect(
-          KicadPCB.addToken({
-            tokens: ["(", "kicad_pcb", "(", ")", ")"],
-            action: START,
-            context: {},
-            property: "",
-            open: 0
-          })
-        ).toEqual({
-          action: CLOSE_CONTEXT,
-          context: { kicad_pcb: {} },
-          property: "kicad_pcb",
-          tokens: [],
-          open: 0
-        });
-      });
+  describe("getLines", () => {
+    it("fines root lines", () => {
+      const lines = [
+        '(kicad_pcb (version 20171130) (host pcbnew "(5.1.0)-1")',
+        "  (general",
+        "    (thickness 1.6)",
+        "    (drawings 0)",
+        "    (tracks 0)",
+        "    (zones 0)",
+        "    (modules 2)",
+        "    (nets 5)",
+        "  )",
+        ")",
+        ""
+      ];
+
+      expect(KicadPCB.getLines(lines).lines).toEqual([
+        '(kicad_pcb (version 20171130) (host pcbnew "(5.1.0)-1")',
+        "section_0",
+        ")",
+        ""
+      ]);
     });
 
-    describe("(kicad_pcb((version 20171130)))", () => {
-      it("kicad_pcb", () => {
-        expect(
-          KicadPCB.addToken({
-            tokens: [
-              /**/ "(",
-              /*  */ "kicad_pcb",
-              /*  */ "(",
-              /*    */ "(",
-              /*      */ "version",
-              /*      */ "20171130",
-              /*    */ ")",
-              /*  */ ")",
-              /**/ ")"
-            ],
-            action: START,
-            context: {},
-            property: "",
-            open: 0
-          })
-        ).toEqual({
-          action: CLOSE_CONTEXT,
-          context: { kicad_pcb: { version: "20171130" } },
-          property: "kicad_pcb",
-          tokens: [],
-          open: 0
-        });
-      });
+    it("finds sections - simple", () => {
+      const lines = [
+        '(kicad_pcb (version 20171130) (host pcbnew "(5.1.0)-1")',
+        "  (general",
+        "    (thickness 1.6)",
+        "    (drawings 0)",
+        "    (tracks 0)",
+        "    (zones 0)",
+        "    (modules 2)",
+        "    (nets 5)",
+        "  )",
+        ")",
+        ""
+      ];
+
+      expect(KicadPCB.getLines(lines).sections).toEqual([
+        [
+          "  (general",
+          "    (thickness 1.6)",
+          "    (drawings 0)",
+          "    (tracks 0)",
+          "    (zones 0)",
+          "    (modules 2)",
+          "    (nets 5)",
+          "  )"
+        ]
+      ]);
     });
 
-    describe("(kicad_pcb((version 20171130) (a 1)))", () => {
-      it("kicad_pcb", () => {
-        expect(
-          KicadPCB.addToken({
-            tokens: [
-              /**/ "(",
-              /*  */ "kicad_pcb",
-              /*  */ "(",
-              /*    */ "(",
-              /*      */ "version",
-              /*      */ "20171130",
-              /*    */ ")",
-              /*    */ "(",
-              /*      */ "a",
-              /*      */ "1",
-              /*    */ ")",
-              /*  */ ")",
-              /**/ ")"
-            ],
-            action: START,
-            context: {},
-            property: "",
-            open: 0
-          })
-        ).toEqual({
-          action: CLOSE_CONTEXT,
-          context: { kicad_pcb: { version: "20171130", a: "1" } },
-          property: "kicad_pcb",
-          tokens: [],
-          open: 0
-        });
-      });
-    });
+    it("get sections - complex", () => {
+      const fixture = "src/models/templates/kicad.pcb.tamplate.kicad_pcb";
+      const raw = readFileSync(fixture, "utf8");
+      const lines = KicadPCB.parseTokens(raw);
 
-    describe("(kicad_pcb((version 20171130) (a 1 2)))", () => {
-      it("kicad_pcb", () => {
-        expect(
-          KicadPCB.addToken({
-            tokens: [
-              /**/ "(",
-              /*  */ "kicad_pcb",
-              /*  */ "(",
-              /*    */ "(",
-              /*      */ "version",
-              /*      */ "20171130",
-              /*    */ ")",
-              /*    */ "(",
-              /*      */ "a",
-              /*      */ "1",
-              /*      */ "2",
-              /*    */ ")",
-              /*  */ ")",
-              /**/ ")"
-            ],
-            action: START,
-            context: {},
-            property: "",
-            open: 0
-          })
-        ).toEqual({
-          action: CLOSE_CONTEXT,
-          context: { kicad_pcb: { version: "20171130", a: ["1", "2"] } },
-          property: "kicad_pcb",
-          tokens: [],
-          open: 0
-        });
-      });
-    });
-
-    describe("(kicad_pcb((version 20171130) (a 1 2 (b 1))))", () => {
-      it.only("kicad_pcb", () => {
-        expect(
-          KicadPCB.addToken({
-            tokens: [
-              /**/ "(",
-              /*  */ "kicad_pcb",
-              /*  */ "(",
-              /*    */ "(",
-              /*      */ "version",
-              /*      */ "20171130",
-              /*    */ ")",
-              /*    */ "(",
-              /*      */ "a",
-              /*      */ "1",
-              /*      */ "(",
-              /*      */ "b",
-              /*      */ "1",
-              /*      */ ")",
-              /*    */ ")",
-              /*  */ ")",
-              /**/ ")"
-            ],
-            action: START,
-            context: {},
-            property: "",
-            open: 0
-          })
-        ).toEqual({
-          action: CLOSE_CONTEXT,
-          context: {
-            kicad_pcb: { version: "20171130", a: ["1", { b: 1 }] }
-          },
-          property: "kicad_pcb",
-          tokens: [],
-          open: 0
-        });
-      });
+      expect(KicadPCB.getLines(lines).lines).toEqual([
+        '(kicad_pcb (version 20171130) (host pcbnew "(5.1.0)-1")',
+        "",
+        "section_0",
+        "  ",
+        "section_1",
+        "",
+        "section_2",
+        "",
+        "section_3",
+        "",
+        "section_4",
+        "",
+        "section_5",
+        "",
+        "section_6",
+        "",
+        ")",
+        ""
+      ]);
     });
   });
-
-  // it("can handle 3 levels deep", () => {
-  //   const rawFile = `(kicad_pcb (version 20171130) (host pcbnew "(5.1.0)-1")
-  //   (general
-  //     (thickness 1.6)
-  //     (drawings 0)
-  //     (tracks 0)
-  //     (zones 0)
-  //     (modules 2)
-  //     (nets 5)
-  //   )`;
-  //   const pcb = new KicadPCB(rawFile);
-  //   expect(pcb.parse()).toEqual({
-  //     kicad_pcb: {
-  //       general: {
-  //         drawings: "0",
-  //         modules: "2",
-  //         nets: "5",
-  //         thickness: "1.6",
-  //         tracks: "0",
-  //         zones: "0"
-  //       },
-  //       host: ["pcbnew", '"(5.1.0)-1"'],
-  //       version: "20171130"
-  //     }
-  //   });
-  // });
-  // it("3 value array", () => {
-  //   // const rawFile = `(kicad_pcb  fp_text value MX-NoLED (at 0 5.08) (layer F.SilkS) hide)`;
-  //   const rawFile = `(kicad_pcb  (fp_text value MX-NoLED at))`;
-  //   const pcb = new KicadPCB(rawFile);
-  //   expect(pcb.parse()).toEqual({
-  //     kicad_pcb: {
-  //       fp_text: ["value", "MX-NoLED", "at"]
-  //     }
-  //   });
-  // });
-  // it("array with mixed objects", () => {
-  //   // const rawFile = `(kicad_pcb  fp_text value MX-NoLED (at 0 5.08) (layer F.SilkS) hide)`;
-  //   const rawFile = `(kicad_pcb  (fp_text value (MX-NoLED at)))`;
-  //   const pcb = new KicadPCB(rawFile);
-  //   expect(pcb.parse()).toEqual({
-  //     kicad_pcb: {
-  //       fp_text: ["value", { "MX-NoLED": "at" }]
-  //     }
-  //   });
-  // });
-  // it("it can parse a pcb a mix array type value", () => {
-  //   const path = "src/models/templates/kicad.pcb.tamplate.kicad_pcb";
-  //   // const rawFile = readFileSync(path, "utf8");
-  //   // const pcb = new KicadPCB(rawFile);
-  // });
 });
--- src/models/KicadPCB.ts
+++ src/models/KicadPCB.ts
@@ -1,177 +1,92 @@
 export default class KicadPCB {
-  static funtionalParse(content: string) {
-    const remainingTokens: Array<string> = flatMap(
-      content.split(/\r?\n/),
-      (line: string) => {
-        line = line.replace(/#.*$/, "");
-        return line.split(/([()]|"(?:\\"|[^"])*")|\s+/).filter(t => !!t);
-      }
-    ).filter(t => t.length > 0);
+  public lines?: Array<string>;
 
-    const { context } = this.functionParseToken({
-      token: remainingTokens.shift() || "",
-      remainingTokens,
-      context: {},
-      lastState: "",
-      lastProperty: "",
-      parentContext: null,
-      parentContextProperty: "",
-      parenCount: 1
-    });
-
-    return context;
+  public static parseTokens(content: string): Array<string> {
+    // return flatMap(content.split(/\r?\n/), (line: string) => {
+    //   line = line.replace(/#.*$/, "");
+    //   return line.split(/([()]|"(?:\\"|[^"])*")|\s+/).filter(t => !!t);
+    // }).filter(t => t.length > 0);
+    return content.split(/\r?\n/);
   }
 
-  public static functionParseToken(params: ParseContext): any {
-    let {
-      token,
-      remainingTokens,
-      context,
-      lastState,
-      lastProperty,
-      parentContext,
-      parentContextProperty,
-      parenCount
-    } = params;
-    const newState = getNewState(lastState, token);
-    if (remainingTokens.length === 0) {
-      return {
-        token: remainingTokens.shift() || "",
-        remainingTokens: remainingTokens || [],
-        context,
-        lastState: newState,
-        lastProperty,
-        parentContext,
-        parentContextProperty
-      };
-    }
+  public static getSection(name: string, tokens: Array<string>): Array<string> {
+    let openCount = 0;
+    let closeCount = 0;
+    let currentName = "";
+    let openCountWhenFound = 0;
+    let closeCountWhenFound = 0;
+    let matchingTokens = [];
+    tokens.forEach((token: string) => {
+      if (token === "(") {
+        openCount++;
 
-    if (newState === ADD_PROPERTY) {
-      context[token] = {};
-      lastProperty = token;
-    }
+        console.log(openCount, closeCount, token);
+      } else if (token === ")") {
+        closeCount++;
 
-    if (newState === NEW_CONTEXT) {
-      context = context[lastProperty];
-      parentContextProperty = lastProperty;
-      lastProperty = "";
-      parentContext = { ...context };
-    }
-
-    if (newState === SET_PROPERTY) {
-      context[lastProperty] = token;
-    }
-
-    if (newState === CLOSE_CONTEXT) {
-      parentContext[parentContextProperty] = context;
-
-      return {
-        token: remainingTokens.shift() || "",
-        remainingTokens,
-        context: { ...parentContext },
-        lastState: newState,
-        lastProperty: parentContextProperty,
-        parentContext: null,
-        parentContextProperty: ""
-      };
-    }
-
-    return this.functionParseToken({
-      token: remainingTokens.shift() || "",
-      remainingTokens: remainingTokens || [],
-      context: context || {},
-      lastState: newState,
-      lastProperty,
-      parentContext,
-      parentContextProperty,
-      parenCount
-    });
-  }
-
-  public static addToken(tokenContext: TokenContext): TokenContext {
-    let tokens = [...tokenContext.tokens];
-    const token = tokens.shift() || "";
-    let action = getNewState(tokenContext.action, token);
-    // console.log(action, token);
-    let context = { ...tokenContext.context } as any;
-
-    let open = tokenContext.open;
-    if (action === NEW_CONTEXT) {
-      open++;
-    } else if (action === CLOSE_CONTEXT) {
-      open--;
-    }
-
-    let property = tokenContext.property;
-
-    if (action === NEW_CONTEXT) {
-      // const reuseContext = tokenContext.context[tokenContext.property];
-      // if (reuseContext) {
-      //   context = tokenContext.context[tokenContext.property];
-      // }
-    }
-    if (action === ADD_PROPERTY) {
-      property = token;
-
-      const {
-        action: child_action,
-        context: child_context,
-        tokens: child_tokens
-      } = this.addToken({
-        tokens,
-        action,
-        context,
-        property,
-        open: 0 // TODO ? what should this be?
-      });
-      const { contextValue } = child_context;
-      // TODO HERE WE NEED TO MERGE response
-      context[token] = contextValue || child_context;
-      action = child_action;
-      tokens = child_tokens;
-    } else if (action === SET_PROPERTY) {
-      return this.addToken({
-        tokens,
-        action,
-        context: { contextValue: token },
-        property,
-        open
-      });
-    } else if (action === CLOSE_CONTEXT) {
-      //todo why is open 1?
-      if (tokenContext.action === CLOSE_CONTEXT) {
-        return {
-          tokens,
-          action,
-          context,
-          property,
-          open
-        };
-      }
-    } else if (action === PUSH_PROPERTY) {
-      let { contextValue } = context;
-      if (Array.isArray(contextValue)) {
-        contextValue = [...contextValue, token];
+        console.log(openCount, closeCount, token);
       } else {
-        contextValue = [contextValue, token];
+        if (token === name) {
+          openCountWhenFound = openCount;
+          closeCountWhenFound = closeCount;
+        }
+        console.log(openCount, closeCount, token);
       }
-      context.contextValue = contextValue;
-    }
-
-    const newTokenContext = {
-      tokens,
-      action,
-      context,
-      property,
-      open
-    };
-
-    if (open > 0) {
-      return this.addToken(newTokenContext);
-    } else {
-      return newTokenContext;
-    }
+    });
+    return [];
   }
+
+  public static parsePCB(raw: string): KicadPCB {
+    const tokens = this.parseTokens(raw);
+    console.log(tokens);
+    return new KicadPCB();
+  }
+
+  public static getLines(lines: Array<string>): Foo {
+    let openCount = 0;
+    let closeCount = 0;
+    let lastLevel = 0;
+    let level = 0;
+    const rootLines: Array<string> = [];
+    let currentSection: Array<string> = [];
+    const sections: Array<Array<string>> = [];
+    let lastRoot = true;
+    let root = true;
+    lines.forEach((line: string, index: number) => {
+      openCount = openCount + (line.match(/\(/g) || []).length;
+      lastLevel = level;
+      level = openCount - closeCount;
+      closeCount = closeCount + (line.match(/\)/g) || []).length;
+      lastRoot = root;
+      root = index === 0 || level <= 1;
+
+      const rootChange = root !== lastRoot;
+
+      if (rootChange && index > 0) {
+        if (currentSection.length > 0) {
+          rootLines.push(`section_${sections.length}`);
+          sections.push(currentSection);
+        }
+        currentSection = [];
+      }
+
+      if (root) {
+        rootLines.push(line);
+      } else {
+        currentSection.push(line);
+      }
+    });
+
+    return {
+      lines: rootLines,
+      sections
+    };
+  }
+}
+
+interface Foo {
+  lines: Array<string>;
+  sections: Array<Array<string>>;
 }
 
 export const NEW_CONTEXT = "NEW_CONTEXT";
@@ -180,51 +95,6 @@ export const SET_PROPERTY = "SET_PROPERTY";
 export const PUSH_PROPERTY = "PUSH_PROPERTY";
 export const CLOSE_CONTEXT = "CLOSE_CONTEXT";
 export const START = "START";
-
-function getNewState(state: string, token: string): string {
-  if (token === "(") {
-    return NEW_CONTEXT;
-  } else if (token === ")") {
-    return CLOSE_CONTEXT;
-  } else if (state === ADD_PROPERTY) {
-    return SET_PROPERTY;
-  } else if (state === SET_PROPERTY) {
-    return PUSH_PROPERTY;
-  } else {
-    return ADD_PROPERTY;
-  }
-}
-interface ParseContext {
-  token: string;
-  remainingTokens: Array<string>;
-  context: any;
-  lastState: string;
-  lastProperty: string;
-  parentContext: any;
-  parentContextProperty: string;
-  parenCount: number;
-}
-
-interface TokenContext {
-  tokens: Array<string>;
-  context: any;
-  action: string;
-  property: string;
-  open: number;
-}
-
-export function getDefaultParseContext(): ParseContext {
-  return {
-    token: "",
-    remainingTokens: [],
-    context: {},
-    lastState: "",
-    lastProperty: "",
-    parentContext: null,
-    parentContextProperty: "",
-    parenCount: 0
-  };
-}
 
 function flatMap<T, U>(
   array: T[],
