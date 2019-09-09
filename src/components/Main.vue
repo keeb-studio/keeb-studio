edad448a9f7c0c5bf96faeef741f8a677a914ccb
Bryan Arendt <barendt@covermymeds.com>
2019-09-09 07:28:48 -0400

    add main component and getter

---
+++ src/components/Main.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    {{ display }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { Getter } from "vuex-class";
+@Component
+export default class Main extends Vue {
+  @Getter("keyset", { namespace: "layout" }) keyset: any;
+  //todo move to vuex getter
+  get display() {
+    return this.keyset.kleParsed.length > 0
+      ? this.keyset.kleParsed[0].name
+      : "Nothing Loaded";
+  }
+}
+</script>
+
+<style></style>
