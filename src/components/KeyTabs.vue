a86cf9460ba3db8cf946155e2336a0acad666cbb
ctrlshiftba <bryanarendt@gmail.com>
2019-10-06 11:01:28 -0400

    starts adding tabs in main view

---
+++ src/components/KeyTabs.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul class="nav nav-tabs m-2">
+    <li class="nav-item" v-for="tab in tabs" :key="tab">
+      <a
+        :class="[navLink, isActive(tab) ? activeClass : '']"
+        @click="setTab(tab)"
+        >{{ tab }}</a
+      >
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { Getter, Mutation } from "vuex-class";
+@Component({})
+export default class KeyTabs extends Vue {
+  @Getter("mainView", { namespace: "editor" }) mainView!: string;
+  @Mutation("setTab", { namespace: "editor" }) setTab!: void;
+  navLink: string = "nav-link";
+  activeClass: string = "active";
+  tabs: string[] = ["Keys", "Plate", "Top"];
+  isActive(tab: string): boolean {
+    return this.mainView === tab;
+  }
+}
+</script>
+
+<style></style>
---
+++ src/components/MainViewPlate.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Plate</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { Getter, Mutation, Action } from "vuex-class";
+
+@Component({})
+export default class MainViewPlate extends Vue {}
+</script>
+
+<style></style>
