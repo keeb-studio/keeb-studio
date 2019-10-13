4772e884f108a9ac255d11a9cc976ba02614367e
ctrlshiftba <bryanarendt@gmail.com>
2019-10-12 20:25:14 -0400

    move toolbar into keyset

--- src/components/Keyset.vue
+++ src/components/Keyset.vue
@@ -1,6 +1,57 @@
 <template>
   <div>
     <KeyTabs />
+
+    <div class="container">
+      <div
+        class="btn-toolbar justify-content-between m-3"
+        role="toolbar"
+        aria-label="Toolbar with button groups"
+        v-if="showToolbar"
+      >
+        <div class="btn-group" role="group" aria-label="First group">
+          <button type="button" class="btn btn-outline-primary" @click="addMx">
+            Add
+          </button>
+
+          <button
+            type="button"
+            class="btn btn-outline-primary mr-2"
+            @click="removeMxSwitch"
+          >
+            Remove
+          </button>
+
+          <button
+            type="button"
+            class="btn btn-outline-primary"
+            @click="selectAll"
+          >
+            Select All
+          </button>
+
+          <button
+            type="button"
+            class="btn btn-outline-primary"
+            @click="toggleMultiSelect"
+          >
+            Multi: {{ multiSelect ? "on" : "off" }}
+          </button>
+        </div>
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <div class="input-group-text" id="btnGroupAddon2">@</div>
+          </div>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Input group example"
+            aria-label="Input group example"
+            aria-describedby="btnGroupAddon2"
+          />
+        </div>
+      </div>
+    </div>
     <MainViewKeys iv v-if="mainView === 'Keys'" />
     <MainViewKeys iv v-if="mainView === 'Grid'" />
     <MainViewPlate iv v-if="mainView === 'Plate'" />
@@ -12,7 +63,7 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import { Getter } from "vuex-class";
+import { Action, Getter, Mutation } from "vuex-class";
 import KeyTabs from "./KeyTabs.vue";
 import MainViewKeys from "./MainViewKeys.vue";
 import MainViewPlate from "./MainViewPlate.vue";
@@ -24,7 +75,33 @@ import MainViewPlate from "./MainViewPlate.vue";
   }
 })
 export default class Keyset extends Vue {
+  @Getter("multiSelect", { namespace: "layout" }) multiSelect: any;
+  @Getter("showToolbar", { namespace: "layout" }) showToolbar: any;
+  @Getter("gridMode", { namespace: "layout" }) gridMode: any;
+
+  @Mutation("toggleMultiSelect", { namespace: "layout" })
+  toggleMultiSelect: any;
+
+  @Mutation("toggleGridMode", { namespace: "layout" }) toggleGridMode: any;
+
+  @Action("addMxSwitch", { namespace: "layout" }) addMxSwitch: any;
+  @Action("removeMxSwitch", { namespace: "layout" }) removeMxSwitch: any;
+  @Action("addMxSwitch", { namespace: "layout" }) addMxSwitch: any;
+  @Action("removeMxSwitch", { namespace: "layout" }) removeMxSwitch: any;
   @Getter("mainView", { namespace: "layout" }) mainView!: string;
+  @Getter("showToolbar", { namespace: "layout" }) showToolbar: any;
+
+  @Mutation("selectAll", { namespace: "layout" }) selectAll: any;
+  @Getter("allKeys", { namespace: "layout" }) allKeys!: Array<SimpleKey>;
+  addMx() {
+    const maxY =
+      this.allKeys.reduce((previosKey: SimpleKey, key: SimpleKey) => {
+        const biggerKey = key.y > previosKey.y ? key : previosKey;
+        return biggerKey;
+      }).y + 1;
+
+    this.addMxSwitch({ x: 0, y: maxY });
+  }
 }
 </script>
 
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,103 +1,20 @@
 <template>
   <header>
     <div id="nav" class="navbar navbar-dark bg-dark">
-      <router-link class="navbar-brand" to="/">Keeb Studio</router-link>
-      <router-link to="/projects" class="btn btn-outline-primary"
-        >Projects</router-link
-      >
+      <router-link class="navbar-brand" to="/">
+        Keeb Studio
+      </router-link>
+      <router-link to="/projects" class="btn btn-outline-primary">
+        Projects
+      </router-link>
       <WriteGist />
-    </div>
-    <div class="container">
-      <div
-        class="btn-toolbar justify-content-between m-3"
-        role="toolbar"
-        aria-label="Toolbar with button groups"
-        v-if="showToolbar"
-      >
-        <div class="btn-group" role="group" aria-label="First group">
-          <button type="button" class="btn btn-outline-primary" @click="addMx">
-            Add
-          </button>
-
-          <button
-            type="button"
-            class="btn btn-outline-primary mr-2"
-            @click="removeMxSwitch"
-          >
-            Remove
-          </button>
-
-          <button
-            type="button"
-            class="btn btn-outline-primary"
-            @click="selectAll"
-          >
-            Select All
-          </button>
-
-          <button
-            type="button"
-            class="btn btn-outline-primary"
-            @click="toggleMultiSelect"
-          >
-            Multi: {{ multiSelect ? "on" : "off" }}
-          </button>
-        </div>
-        <div class="input-group">
-          <div class="input-group-prepend">
-            <div class="input-group-text" id="btnGroupAddon2">@</div>
-          </div>
-          <input
-            type="text"
-            class="form-control"
-            placeholder="Input group example"
-            aria-label="Input group example"
-            aria-describedby="btnGroupAddon2"
-          />
-        </div>
-      </div>
-      <div v-else id="spaceholder"><div></div></div>
     </div>
   </header>
 </template>
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import WriteGist from "./SavedGists/WriteGist.vue";
-import { Getter, Mutation, Action } from "vuex-class";
-import { SimpleKey } from "@/models/KeysetLayout/SimpleKey";
 @Component({ components: { WriteGist } })
-export default class Nav extends Vue {
-  @Getter("multiSelect", { namespace: "layout" }) multiSelect: any;
-  @Getter("showToolbar", { namespace: "layout" }) showToolbar: any;
-  @Getter("gridMode", { namespace: "layout" }) gridMode: any;
-
-  @Mutation("selectAll", { namespace: "layout" }) selectAll: any;
-
-  @Mutation("toggleMultiSelect", { namespace: "layout" })
-  toggleMultiSelect: any;
-
-  @Mutation("toggleGridMode", { namespace: "layout" }) toggleGridMode: any;
-
-  @Action("addMxSwitch", { namespace: "layout" }) addMxSwitch: any;
-  @Action("removeMxSwitch", { namespace: "layout" }) removeMxSwitch: any;
-
-  @Getter("allKeys", { namespace: "layout" }) allKeys!: Array<SimpleKey>;
-  addMx() {
-    const maxY =
-      this.allKeys.reduce((previosKey: SimpleKey, key: SimpleKey) => {
-        const biggerKey = key.y > previosKey.y ? key : previosKey;
-        return biggerKey;
-      }).y + 1;
-
-    this.addMxSwitch({ x: 0, y: maxY });
-  }
-}
+export default class Nav extends Vue {}
 </script>
-<style lang="scss" scoped>
-#spaceholder {
-  margin: 16px;
-  div {
-    height: 38px;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
